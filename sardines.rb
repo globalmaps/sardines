ba6001ab40b87dc7dee95614cfcaa9b4ea4e40d4
Hidenori FUJIMURA <hfu@si.local>
2016-09-24 05:18:34 +0900

    update

--- sardines.rb
+++ sardines.rb
@@ -1,5 +1,7 @@
 require 'find'
+LIMIT = 85 * 1024 * 1024
 
+large_files = []
 Find.find('.') {|path|
   if %w{Read_me.txt Thumbs.db}.include?(File.basename(path))
     print "rm #{path}\n"
@@ -11,4 +13,23 @@ Find.find('.') {|path|
       print "rm #{path}\n"
     end
   end
+  if(File.size(path).to_f  / LIMIT > 1)
+    print "split -b 85m #{path} #{path}.\n"
+    print "rm #{path}\n"
+    large_files << path
+  end
 }
+if(large_files.size > 0)
+  File.open('.gitignore', 'w') {|w|
+    large_files.each {|path|
+      w.print path, "\n"
+    }
+  }
+  $stderr.print ".gitignore written.\n"
+  File.open('unsplit.sh', 'w') {|w|
+    large_files.each {|path|
+      w.print "cat #{path}* > #{path}\n"
+    }
+  }
+  $stderr.print "unsplit.sh written.\n"
+end
